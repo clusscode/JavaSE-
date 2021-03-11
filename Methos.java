8c054df47ebb0688436bc8bb108f0b7d492309dc
clusscode <77377776+clusscode@users.noreply.github.com>
2021-03-11 10:21:57 +0800

    奇数在前偶数在后

--- Methos.java
+++ Methos.java
@@ -1,19 +1,52 @@
 package Java41_2_9;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.Scanner;
 
 public class Methos {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        System.out.println(factorial(N));
+        int[] arr = new int[10];
+        InputArray(arr);
+        Exchange(arr);
+        Print(arr);
     }
 
-    private static int factorial(int N) {
-        int factorial = 1;
-        for(int i = 1; i <= N; i++){
-            factorial *= i;
+    private static void Print(@NotNull int[] arr) {
+        for(int x : arr){
+            System.out.print(x + "");
         }
-        return factorial;
+    }
+
+    public static int[] InputArray(int[] arr){
+        Scanner scanner = new Scanner(System.in);
+        for(int i= 0; i < arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+    public static int[] Exchange(int[] arr){
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right){
+            while(left < right){
+                if(arr[left] % 2 == 0){
+                    break;
+                }
+                left++;
+            }
+            while(left < right){
+                if(arr[right] % 2 != 0){
+                    break;
+                }
+                right--;
+            }
+            if(left < right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+        return arr;
     }
 }
